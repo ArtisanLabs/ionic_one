de63d27bee3de6c1db25db0228eb4e778c87df4d
ArtisanLabs <artisan254@gmail.com>
2018-02-15 18:50:47 +0300

    Initial Commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+  username: string;
+  password: string;
+  credentials: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  authUser(){
+  	this.credentials = {
+  		'username': this.username,
+		'password': this.password
+	};
+  	
+  	console.log(this.credentials);
+  }
+
+}
